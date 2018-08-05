9d2cbd872fed855238e9b82e2beb603e9b4974f0
EIMIKI <kou.103.kan@gmail.com>
2018-08-05 21:01:45 +0900

    データベース更新用関数を作成

---
+++ db/dbUpdate.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"ComicUpdateViewer/getPages"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func Update(conn *sql.DB, comic getPages.TodayUpdate, date string) error {
+	_, err := conn.Exec("update comics set date=" + date + " img='" + comic.ImgUrl + "' where title='" + comic.Title + "'")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Add(conn *sql.DB, comic getPages.TodayUpdate, date string) error {
+	_, err := conn.Exec("insert into comics (title,url,img,date) value ('" + comic.Title + "','" + comic.Url + "','" + comic.ImgUrl + "'," + date + ")")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Push(conn *sql.DB, comics []getPages.TodayUpdate) error {
+	var exists string
+	for _, comic := range comics {
+		err := conn.QueryRow("select exists (select * from comics where title='" + comic.Title + "')").Scan(exists)
+		if err != nil {
+			return err
+		}
+
+		now := time.Now()
+		date := fmt.Sprintf("%d-%d-%d", now.Year(), now.Month(), now.Day())
+		if err == sql.ErrNoRows {
+			Add(conn, comic, date)
+		} else {
+			Update(conn, comic, date)
+		}
+	}
+	return nil
+}
