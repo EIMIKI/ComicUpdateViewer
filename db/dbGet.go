5c2c2d8813174af1139b5b2ad31bfab4e45dcb90
EIMIKI <kou.103.kan@gmail.com>
2018-08-07 20:46:15 +0900

    当日の更新をDBから取ってくる関数を容易

--- db/dbGet.go
+++ db/dbGet.go
@@ -1,1 +1,39 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Result struct {
+	ComicId string
+	Title   string
+	Url     string
+	Img     string
+}
+
+// DBから実行した時点での日付のついたデータを持ってくる
+func GetTodayUpdate(conn *sql.DB) error {
+	date := GetDate()
+	resultRows, err := conn.Query("select comic_id,title,url,img from comics where date='" + date + "'")
+	if err != nil {
+		return err
+	}
+
+	defer resultRows.Close()
+
+	results := []Result{}
+	for resultRows.Next() {
+		result := Result{}
+
+		err := resultRows.Scan(&(result.ComicId), &(result.Title), &result.Url, &result.Img)
+		if err != nil {
+			return err
+		}
+		results = append(results, result)
+	}
+
+	fmt.Println(results)
+
+	return nil
+}
